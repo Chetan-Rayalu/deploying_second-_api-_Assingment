eadba120a5b13347408095e0617fbb3af72b1d23
Chetan Rayalu <$ chetanrayaluthirumalasetty@gmail.com>
2025-02-11 10:42:13 +0530

    added files

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const { resolve } = require('path');
 
+require('dotenv').config();
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("✅ Admin privileges granted.");
+} else {
+  console.log("⛔ Access restricted. Admin only.");
+}
+
+
+
 const app = express();
 const port = 3010;
 
